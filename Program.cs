8a9b43187401547db59fad7e204a33a8e1ab72ad
Ilya Ustinov <ustin_1999@mail.ru>
2023-06-24 12:53:09 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите количество элементов в массиве: ");
+int count = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Построчно введите элементы массива");
+for(int i=0; i<count; i++)
+{
+
+}
